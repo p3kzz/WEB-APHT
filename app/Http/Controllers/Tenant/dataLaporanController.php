f7ac96965e8cbb4163adeb8121e352c09dff3de3
achnurilhams <achnurilhamsetyawan@gmail.com>
2025-07-27 15:20:28 +0700

    blade baru

---
+++ app/Http/Controllers/Tenant/dataLaporanController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Tenant;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class dataLaporanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('tenan.dataLaporan');
+    }
+
+    public function datLaporanView(){
+        return view('tenan.dataProduksi');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/views/layouts/tenant.blade.php
+++ resources/views/layouts/tenant.blade.php
@@ -84,11 +84,11 @@
                                 class="flex items-center space-x-3 p-2 rounded-lg text-gray-700 hover:bg-green-50 hover:text-green-500 transition-colors duration-200">
                                 <span>Laporan Produksi</span>
                             </a>
-                            <a href="dataPengajuan"
+                            <a href="laporankeuangan"
                                 class="flex items-center space-x-3 p-2 rounded-lg text-gray-700 hover:bg-green-50 hover:text-green-500 transition-colors duration-200">
                                 <span>Laporan Keuangan</span>
                             </a>
-                            <a href="dataPengajuan"
+                            <a href="datalaporan"
                                 class="flex items-center space-x-3 p-2 rounded-lg text-gray-700 hover:bg-green-50 hover:text-green-500 transition-colors duration-200">
                                 <span>Data Laporan</span>
                             </a>
@@ -108,7 +108,7 @@
                                 class="w-full h-full object-contain"></div>
                         <span>Data Produksi</span>
                     </a>
-                    <a href="#"
+                    <a href="monitoring"
                         class="flex items-center space-x-3 p-2 rounded-lg text-gray-800 hover:bg-green-100 hover:text-green-600 font-semibold transition-colors duration-200">
                         <div class="w-5 h-5 flex items-center justify-center"><img
                                 src="{{ asset('assets/img/monitoring-system.png') }}" alt="Data Monitoring Icon"
